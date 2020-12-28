29d7ff861f51bc0aa807a587c8194899afdaed51
root <root@host-192-168-191-140.(none)>
2020-12-28 13:45:19 +0800

    add tree level travel

---
+++ levelOrder.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <limits>
+#include "dbg-macro/dbg.h"
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector <vector <int>> ret;
+        if (!root) {
+            return ret;
+        }
+
+        queue <TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int currentLevelSize = q.size();
+            ret.push_back(vector <int> ());
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                auto node = q.front(); q.pop();
+                ret.back().push_back(node->val);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+        
+        return ret;
+    }
+	
+};
+int main(int argc,char* argv[]){
+	Solution s;
+	TreeNode* a = new TreeNode('a');
+	TreeNode* b = new TreeNode('b');
+	TreeNode* c = new TreeNode('c');
+	TreeNode* d = new TreeNode('d');
+	TreeNode* e = new TreeNode('e');
+	TreeNode* f = new TreeNode('f');
+	TreeNode* g = new TreeNode('g');
+	TreeNode* h = new TreeNode('h');
+	TreeNode* i = new TreeNode('i');
+	
+	f->left = b;
+	b->left = a;
+	b->right=d;
+	d->left = c;
+	d->right = e;
+	f->right = g;
+	g->right = i;
+	i->left = h;
+	auto result = s.levelOrder(f);
+	dbg(result);
+	return 0;
+}
